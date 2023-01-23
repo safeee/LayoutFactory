68fd81082f2cc4ecfb2cc43040dcdf3126320ba1
safa chaouch <chaouch.safee@gmail.com>
2023-01-23 14:46:52 +0100

    first version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FactoryComponent } from './factory/factory.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FactoryComponent
   ],
   imports: [
     BrowserModule,
